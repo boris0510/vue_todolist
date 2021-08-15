5124c0ba9430ac35317a5f9a4600c893c675dd09
Boris Huang <scps880510@gmail.com>
2021-08-15 11:29:51 +0800

    修正畫面

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,7 @@
       <div class="input-group-prepend">
         <span class="input-group-text" id="basic-addon1">事項</span>
       </div>
-      <input
-        type="text"
-        class="form-control"
-        placeholder="輸入代辦事項"
-        v-model="newTodo"
-        @keyup.enter="addTodo"
-      />
+      <input type="text" class="form-control" placeholder="輸入代辦事項" v-model="newTodo" @keyup.enter="addTodo" />
       <div class="input-group-append">
         <button class="btn btn-primary" type="button" @click="addTodo">
           新增事項
@@ -22,83 +16,38 @@
       <div class="card-header">
         <ul class="nav nav-tabs card-header-tabs">
           <li class="nav-item">
-            <a
-              class="nav-link"
-              href="#"
-              :class="{ active: visibility == 'all' }"
-              @click="visibility = 'all'"
-              >全部</a
-            >
+            <a class="nav-link" href="#" :class="{ active: visibility == 'all' }" @click.prevent="visibility = 'all'">全部 ({{ data.length }})</a>
           </li>
           <li class="nav-item">
-            <a
-              class="nav-link"
-              href="#"
-              :class="{ active: visibility == 'active' }"
-              @click="visibility = 'active'"
-              >進行中</a
-            >
+            <a class="nav-link" href="#" :class="{ active: visibility == 'active' }" @click.prevent="visibility = 'active'">進行中 ({{ undone }})</a>
           </li>
           <li class="nav-item">
-            <a
-              class="nav-link"
-              href="#"
-              :class="{ active: visibility == 'completed' }"
-              @click="visibility = 'completed'"
-              >已完成</a
-            >
+            <a class="nav-link" href="#" :class="{ active: visibility == 'completed' }" @click.prevent="visibility = 'completed'">已完成 ({{ data.length - undone }})</a>
           </li>
         </ul>
       </div>
       <ul class="list-group list-group-flush text-left">
-        <li
-          class="list-group-item"
-          v-for="(item, key) in filteredTodos"
-          :key="key"
-          @dblclick="editTodo(item)"
-        >
+        <li class="list-group-item" v-for="(item, key) in filteredTodos" :key="key" @dblclick="editTodo(item)">
           <div class="d-flex" v-if="item.id !== cacheTodo.id">
             <div class="d-flex align-items-center">
-              <button
-                class="btn btn-primary btn-sm me-3"
-                @click="setCompleted(item)"
-                :id="item.id"
-                v-if="!item.completed"
-              >
+              <button class="btn btn-primary btn-sm me-3" @click="setCompleted(item)" :id="item.id" v-if="!item.completed">
                 確認完成
               </button>
-              <button
-                class="btn btn-danger btn-sm me-3"
-                @click="setCompleted(item)"
-                :id="item.id"
-                v-if="item.completed"
-              >
+              <button class="btn btn-danger btn-sm me-3" @click="setCompleted(item)" :id="item.id" v-if="item.completed">
                 取消完成
               </button>
               <div :for="item.id" :class="{ completed: item.completed }">
                 {{ item.title }}
               </div>
             </div>
-            <button
-              type="button"
-              class="btn-close ms-auto"
-              aria-label="Close"
-              @click="removeTodo(item)"
-            ></button>
+            <button type="button" class="btn-close ms-auto" aria-label="Close" @click="removeTodo(item)"></button>
           </div>
-          <input
-            type="text"
-            class="form-control"
-            v-if="item.id === cacheTodo.id"
-            v-model="cacheTitle"
-            @keyup.esc="cancelEdit()"
-            @keyup.enter="doneEdit(item)"
-          />
+          <input type="text" class="form-control" v-if="item.id === cacheTodo.id" v-model="cacheTitle" @keyup.esc="cancelEdit()" @keyup.enter="doneEdit(item)" />
         </li>
       </ul>
-      <div class="card-footer d-flex justify-content-between">
+      <div class="card-footer d-flex justify-content-between align-items-center">
         <span>還有 {{ undone }} 筆事項尚未完成</span>
-        <a href="#" @click.prevent="clearAll">清除所有事項</a>
+        <a href="#" @click.prevent="clearAll" class="btn btn-outline-danger">清除所有事項</a>
       </div>
     </div>
   </div>
